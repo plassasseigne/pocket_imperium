86c1aad860684b4cfca11e9b512db60f26d8ca04
Paul Lassasseigne <paul@digital-cover.fr>
2025-01-12 01:10:47 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module fr.lasschko.pocketimperium.pocketimperium {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens fr.lasschko.pocketimperium.pocketimperium to javafx.fxml;
+    exports fr.lasschko.pocketimperium.pocketimperium;
+}
